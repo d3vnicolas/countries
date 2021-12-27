efc886aa8596be8d5b5f1e1829eaeee15438b7cc
Nicolas Soares <devnicolas.coroa@gmail.com>
2021-12-27 16:40:40 -0300

    componente header

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <Head>
+            <h1>
+                Where in the world?
+            </h1>
+            <div>
+                <img />
+                <p>
+                    Dark Mode
+                </p>
+            </div>
+        </Head>
+    );
+}
+ 
+export default Header;
